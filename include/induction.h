d28472dea95d4fe4f2081d2d823b96ef0b133cf9
Kurt M. Weber <weberk294@gmail.com>
2025-02-05 21:01:46 -0600

    set up test definition infrastructure

---
+++ include/induction.h
@@ -0,0 +1,16 @@
+/*
+ * Creator: Kurt M. Weber
+ * Created on: 2025-02-05
+ *
+ * This file is part of induction, a component of the Cosmoverse.
+ * Licensed under Hippocratic License with clauses:
+ * HL3-CL-ECO-EXTR-FFD-MEDIA-MY-SUP-SV-TAL-USTA-XUAR
+ * See file LICENSE for full licensing information.
+ */
+
+typedef bool (*induction_test)();
+
+typedef struct _induction_test_set {
+} _induction_test_set;
+
+#define BEGIN_TEST_SET
